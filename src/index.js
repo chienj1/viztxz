dda8d8798a7e918fb73ab1812b4e21f2f5c07d76
chienj1 <24452445+chienj1@users.noreply.github.com>
2022-04-28 17:06:18 +0800

    set up dev env

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const example_file = './example_json/tx-0xa79E63e78Eec28741e711f89A672A4C40876Ebf3-cqt_api.json';
+fetch(example_file)
+  .then(response => response.json())
+  .then(json => console.log(json));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
